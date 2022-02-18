54c00f0701c48516e55eee30ad6da422cd44ed5f
Davin Phuong <davinphuong@yahoo.com>
2022-02-18 14:53:10 -0800

    Add form event listener and create entry function.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* global data */
+/* exported data */
+
 var $addEntry = document.querySelector('#add-entry');
 var $modal = document.querySelector('#modal');
 
@@ -7,10 +10,27 @@ function showModal(event) {
 
 var $submitButton = document.querySelector('#submit-button');
 
-$submitButton.addEventListener('click', closeModal);
+$submitButton.addEventListener('submit', closeModal);
 
 $addEntry.addEventListener('click', showModal);
 
 function closeModal(event) {
   $modal.className = 'black-bg hidden';
+}
+
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', createEntry);
+
+function createEntry(event) {
+  event.preventDefault();
+
+  var entryObj = {
+    day: $form.elements.day.value,
+    time: $form.elements.time.value,
+    description: $form.elements.description.value,
+    entryId: data.nextEntryId
+  };
+
+  data.entries.unshift(entryObj);
 }
